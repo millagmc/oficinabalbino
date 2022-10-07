7ae6d19baa57f2ce52785774b676c66c6c54d346
Camilla Santos <milla.revolts@gmail.com>
2022-10-08 00:33:50 +0100

    landing page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.scss';
 import Header from './Components/Header/Header';
+import LandingPage from './Components/Landing/Landing';
 
 function App() {
   return (
     <div className="App">
-     <Header></Header>
+      <LandingPage></LandingPage>
     </div>
   );
 }
