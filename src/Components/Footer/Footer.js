dec7a655ef93e9a6ca57fab6dc11453f5ff53886
Camilla Santos <milla.revolts@gmail.com>
2022-10-08 01:27:38 +0100

    footer h2 added

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css'
 const Footer = () => {
     return ( 
         <div className="footer">
-            Siga-nos nas nossas redes
+           <h2>Siga-nos nas nossas redes</h2> 
         </div>
      );
 }
