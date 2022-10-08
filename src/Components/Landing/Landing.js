4c1dc492f5b0f19025241b908ad9d9eb05fd86a6
Camilla Santos <milla.revolts@gmail.com>
2022-10-08 01:07:23 +0100

    landing page version 2

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -4,6 +4,7 @@ import './Landing.css'
 const LandingPage = () => {
     return ( 
         <div className="landing--container">
+        <h2>O Medico Do Seu Carro !</h2>
         <div className='button--container'>
         <button className='button--landing'>Visite a loja</button>
         <button className='button--landing'>Fale Conosco</button>  
